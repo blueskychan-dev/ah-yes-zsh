bca822f359d12e60498fba4cd3168a6aa42f6c3b
Simao Gomes Viana <xdevs23@outlook.com>
2019-10-07 22:29:15 +0200

    Another bug bites the dust

--- YEZ-PLZ-UNINSTALL-THIZ-AND-EVERYTHING-ASSOCIATED-TO-IT-BE-WARNED-IT-DELETES-STUFF.sh
+++ YEZ-PLZ-UNINSTALL-THIZ-AND-EVERYTHING-ASSOCIATED-TO-IT-BE-WARNED-IT-DELETES-STUFF.sh
@@ -4,3 +4,4 @@ cd $HOME
 rm -f .zshrc .commonrc .p10k.zsh
 rm -rf .oh-my-zsh ah-yes-zsh
 
+echo "cd out of dis dir"
--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cd ah-yes-zsh
 yay -S --noconfirm zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+zsh -c "git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k"
 
 ./install.zsh
 sudo chsh -s /bin/zsh
