26616e6e39e4e8aded428bb9b20d66bc0cfd2e8d
Simao Gomes Viana <xdevs23@outlook.com>
2019-10-07 22:26:59 +0200

    changed things...

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ cd ah-yes-zsh
 yay -S --noconfirm zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 ./install.zsh
 sudo chsh -s /bin/zsh
