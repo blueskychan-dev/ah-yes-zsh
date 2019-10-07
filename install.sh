99dd194c132e4aaeed275525dc347cf8884ed50a
Simao Gomes Viana <xdevs23@outlook.com>
2019-10-07 22:23:25 +0200

    I'm guessing this may start causing us problems either soon or never.

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ cd $HOME
 git clone https://gitlab.com/xdevs23/ah-yes-zsh
 cd ah-yes-zsh
 yay -S --noconfirm zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 ./install.zsh
 sudo chsh -s /bin/zsh
+zsh
+
 
